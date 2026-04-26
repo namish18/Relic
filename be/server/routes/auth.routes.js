d8eba7159bf411dbe9eb8fa0dc9374aa838061b1
namish18 <namishsahu2022@gmail.com>
2026-04-26 11:52:48 +0530

    auth route added

--- be/server/routes/auth.routes.js
+++ be/server/routes/auth.routes.js
@@ -1,9 +1,14 @@
 import express from 'express';
-import { generateNonce, verifySignatureAndLogin } from '../controllers/auth.controller.js';
+import {
+  generateNonce,
+  verifySignatureAndLogin,
+  privyAuth,
+} from '../controllers/auth.controller.js';
 
 const router = express.Router();
 
 router.post('/nonce', generateNonce);
 router.post('/verify', verifySignatureAndLogin);
+router.post('/privy', privyAuth);
 
 export default router;
